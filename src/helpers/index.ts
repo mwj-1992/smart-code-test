878e730c051b6445f64618a767550f40aaa30412
mwj-1992 <ite.mwj1992@gmail.com>
2023-02-06 16:12:31 +0800

    Added two functions to manipulate query params in URL

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -8,3 +8,35 @@ export const debounce = (func: any, timeout = 300) => {
   };
 };
 
+export const insertUrlParam = (key: string, value: string | number) => {
+  let searchParams = new URLSearchParams(window.location.search);
+  searchParams.set(key, value as string);
+  let newurl =
+    window.location.protocol +
+    "//" +
+    window.location.host +
+    window.location.pathname +
+    "?" +
+    searchParams.toString();
+  window.history.pushState({ path: newurl }, "", newurl);
+};
+
+// to remove the specific key
+export const getUrlParameters = () => {
+  let url = new URL(window.location.href);
+  let params:any = new URLSearchParams(url.search);
+  let object :any = {};
+  for (const [key, value] of params) {
+    object[key] = value;
+  }
+  return object;
+};
+
+export const queryParamsString = (params: any) => {
+  const queryString: string = Object.keys(params)
+    .map(
+      (key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`
+    )
+    .join("&");
+  return queryString;
+};
